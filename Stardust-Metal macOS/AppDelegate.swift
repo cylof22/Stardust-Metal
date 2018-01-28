1dbed94bb40176b7cfb3f0e78dbeecf1fe700713
cylof22 <cylof22@gmail.com>
2018-01-28 21:06:26 +0800

    Initial Commit

---
+++ Stardust-Metal macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Stardust-Metal macOS
+//
+//  Created by 曹元乐 on 2018/1/28.
+//  Copyright © 2018年 曹元乐. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
